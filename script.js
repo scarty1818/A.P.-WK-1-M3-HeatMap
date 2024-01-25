5abafb8a93de3e943771ef6aba6795a779931d61
scarty1818 <156967352+scarty1818@users.noreply.github.com>
2024-01-24 21:45:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var map = L.map('map').setView([37.7, -122.4], 10);
+
+var Stamen_Terrain = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg', {
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+}).addTo(map);
+
+$.getJSON('https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson', function(data) {
+  var coordinatesOnly = data.features.map(function(feature) {
+    return [feature.geometry.coordinates[1], feature.geometry.coordinates[0], 1];
+  });
+
+  var heat = L.heatLayer(coordinatesOnly).addTo(map);
+});
 
